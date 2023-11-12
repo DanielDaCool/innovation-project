a4b75cc8d7ad69065daab11a5152cdd0d6fd9219
nanonoam <97607867+nanonoam@users.noreply.github.com>
2023-11-12 20:21:21 +0200

    checking in workshop ntcore and yolov8 - added no_grad

--- networktablesGlazer.py
+++ networktablesGlazer.py
@@ -1,6 +1,19 @@
 import ntcore
+from ultralytics import YOLO
+import torch
 
 inst = ntcore.NetworkTableInstance.getDefault()
+
+import cv2
+import math 
+# import ntcore
+
+
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
 
 inst.startClient4("example client")
 
@@ -17,7 +30,25 @@ table = inst.getTable("datable")
 x = 2
 y = 1
 
+# model
+model = YOLO('yolov8n.pt')
+
+# object classes
+classNames = ["cubeholder"
+              ]
+
+print("ok2")
+
+
 while True:
+    success, img = cap.read()
+    with torch.no_grad():
+        results = model(img, stream=True)
+        print('results', results)
+        print('img', img[1, 2])
+
+    print("ok3")
+
     xPub = table.getEntry("x")
     yPub = table.getEntry("y")
     aPub = table.getEntry("angle2")
@@ -29,3 +60,47 @@ while True:
     aPub.setDouble(x)
     x += 0.05
     y += 1.0
+
+    # coordinates
+    for r in results:
+        print("ok4")
+        boxes = r.boxes
+
+        for box in boxes:
+            print("ok5")
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+            print("x1", x1)
+            print("x2", x2)
+            print("y1", y1)
+            print("y2", y2)
+            
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
